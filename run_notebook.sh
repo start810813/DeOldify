3f4c9c48a7012d070777477c0a1d1028b0ad75f1
陳融威 <start810813@gmail.com>
2019-06-18 18:07:32 +0800

    commit

--- run_notebook.sh
+++
@@ -1,1 +0,0 @@
-jupyter notebook --port=8888 --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token="" --NotebookApp.password="$(python set_password.py $NOTEBOOK_PASSWORD)"
--- set_password.py
+++
@@ -1,4 +0,0 @@
-import sys
-from IPython.lib import passwd
-password = passwd(sys.argv[1])
-print(password)
